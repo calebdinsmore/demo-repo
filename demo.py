99fa1c25d5ddc4376e4efc572b55a0c77df43ad8
cjdinsmore <caleb_dinsmore@taylor.edu>
2016-03-11 12:19:26 -0500

    Made a better homepage

--- demo.py
+++ demo.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('myhomepage')
+def homepage():
+    return "This is MY homepage!"
+
 
 if __name__ == '__main__':
     app.run()
