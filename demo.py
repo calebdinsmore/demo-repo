51fb70a9202fcc7908dffdbbaee7c037f25ab055
cjdinsmore <caleb_dinsmore@taylor.edu>
2016-03-11 12:12:48 -0500

    Made  a thing

--- demo.py
+++ demo.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/homepage')
+def homepage():
+    return 'This is the homepage!'
+
 
 if __name__ == '__main__':
     app.run()
